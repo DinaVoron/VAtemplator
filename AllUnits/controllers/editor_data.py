b6b54b0a86b0ccda2e065098713046623d19cff6
Voronova Diana <i@dinavoron.ru>
2024-04-17 02:35:22 +1000

    Сверстать страницу статистики по макету, изменить размер меню

--- AllUnits/controllers/editor_data.py
+++ AllUnits/controllers/editor_data.py
@@ -11,10 +11,14 @@ def editor_data():
     not_found_amount = get_nf_num()
     error_amount = get_err_num()
     all_amount = success_amount + not_found_amount + error_amount
+    start_date = None
+    end_date = None
+    if request.values.get("start_date") != '':
+        start_date = request.values.get("start_date")
+    if request.values.get("end_date") != '':
+        end_date = request.values.get("end_date")
 
     errs_per_scene = count_errors()
-
-    time = get_time()
 
     if request.values.get("open_ok"):
         subprocess.Popen(["notepad", "logs/OK.log"])
@@ -25,18 +29,20 @@ def editor_data():
     if request.values.get("open_nf"):
         subprocess.Popen(["notepad", "logs/NF.log"])
 
-    intents = graph_verify(dialog_tree, graph)
+    print("start_date")
+    print(start_date)
+    print("end_date")
+    print(end_date)
+    time = get_time(start_date, end_date)
 
     html = render_template(
         "editor_data.html",
         current_page='editor_data',
-
         success_amount=success_amount,
         not_found_amount=not_found_amount,
         error_amount=error_amount,
         all_amount=all_amount,
         errs_per_scene=errs_per_scene,
-        intents=intents,
         time=time,
         len=len,
         int=int
